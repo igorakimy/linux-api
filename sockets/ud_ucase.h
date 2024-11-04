e7e185a1b4cab74aa16361a4875b06de9c2ee9ee
igorakimy <igorakimy@gmail.com>
2024-11-04 12:17:01 +0300

    chore: ipv6 datagram sockets formatting

--- sockets/ud_ucase.h
+++ sockets/ud_ucase.h
@@ -1,9 +1,8 @@
-#include <sys/un.h>
-#include <sys/socket.h>
 #include <ctype.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 #include "../lib/tlpi_hdr.h"
 
-#define BUF_SIZE 10         /* Максимальный размер сообщений,
-                               которыми обмениваются клиент и сервер */
+#define BUF_SIZE     10 /* Максимальный размер сообщений, которыми обмениваются клиент и сервер */
 
 #define SV_SOCK_PATH "/tmp/ud_ucase"
